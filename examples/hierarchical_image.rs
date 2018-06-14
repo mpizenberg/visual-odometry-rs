d7ed8974537d03c090f2907e9bce68acff89ea14
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-06-14 18:14:48 +0800

    Alternative image_from_matrix

--- examples/hierarchical_image.rs
+++ examples/hierarchical_image.rs
@@ -20,13 +20,17 @@ fn main() {
     let img_matrix = DMatrix::from_row_slice(height as usize, width as usize, &raw_buffer);
     let img_matrix_2 = half_resolution(&img_matrix).unwrap();
     let img_matrix_4 = half_resolution(&img_matrix_2).unwrap();
+    let img_matrix_8 = half_resolution(&img_matrix_4).unwrap();
 
     // save half resolution image.
-    image_from_matrix(&img_matrix_2)
+    image_from_matrix_1(&img_matrix_2)
         .save("out/gray_2.png")
         .unwrap();
-    image_from_matrix(&img_matrix_4)
+    image_from_matrix_2(&img_matrix_4)
         .save("out/gray_4.png")
+        .unwrap();
+    image_from_matrix_3(&img_matrix_8)
+        .save("out/gray_8.png")
         .unwrap();
 }
 
@@ -47,11 +51,39 @@ fn half_resolution(mat: &DMatrix<u8>) -> Option<DMatrix<u8>> {
     }
 }
 
-fn image_from_matrix(mat: &DMatrix<u8>) -> GrayImage {
+// to_luma() creates a copy of the buffer.
+fn image_from_matrix_1(mat: &DMatrix<u8>) -> GrayImage {
     let (nb_rows, nb_cols) = mat.shape();
     let mut img_buf = DynamicImage::new_luma8(nb_cols as u32, nb_rows as u32).to_luma();
     for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
         *pixel = image::Luma([mat[(y as usize, x as usize)]]);
     }
     img_buf
+}
+
+// I'm not sure what's the memory management for as_luma8().unwrap().to_owned().
+fn image_from_matrix_2(mat: &DMatrix<u8>) -> GrayImage {
+    let (nb_rows, nb_cols) = mat.shape();
+    let mut img_buf = DynamicImage::new_luma8(nb_cols as u32, nb_rows as u32)
+        .as_luma8()
+        .unwrap()
+        .to_owned();
+    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
+        *pixel = image::Luma([mat[(y as usize, x as usize)]]);
+    }
+    img_buf
+}
+
+// I think that's the only version not copying the buffer but not sure.
+fn image_from_matrix_3(mat: &DMatrix<u8>) -> GrayImage {
+    let (nb_rows, nb_cols) = mat.shape();
+    match DynamicImage::new_luma8(nb_cols as u32, nb_rows as u32) {
+        DynamicImage::ImageLuma8(mut gray_img) => {
+            for (x, y, pixel) in gray_img.enumerate_pixels_mut() {
+                *pixel = image::Luma([mat[(y as usize, x as usize)]]);
+            }
+            gray_img
+        }
+        _ => panic!("lol"),
+    }
 }
