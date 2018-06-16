9a0aca1ccb999ab91d3927310393ecbfc13b9f6a
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-06-16 17:22:54 +0800

    Replace slices by for loops

--- examples/gradient.rs
+++ examples/gradient.rs
@@ -43,13 +43,9 @@ fn gradient_x(mat: &DMatrix<u8>) -> DMatrix<i16> {
     let (nb_rows, nb_cols) = mat.shape();
     let mut grad_x = DMatrix::zeros(nb_rows, nb_cols);
     if nb_rows >= 2 && nb_cols >= 2 {
-        let inner_grad_x = mat.slice((1, 2), (nb_rows - 2, nb_cols - 2)).zip_map(
-            &mat.slice((1, 0), (nb_rows - 2, nb_cols - 2)),
-            |x_2, x_0| x_2 as i16 - x_0 as i16,
-        );
-        for c in 0..(nb_cols - 2) {
-            for r in 0..(nb_rows - 2) {
-                grad_x[(r + 1, c + 1)] = inner_grad_x[(r, c)];
+        for c in 1..(nb_cols - 1) {
+            for r in 1..(nb_rows - 1) {
+                grad_x[(r, c)] = mat[(r, c + 1)] as i16 - mat[(r, c - 1)] as i16;
             }
         }
     }
@@ -60,13 +56,9 @@ fn gradient_y(mat: &DMatrix<u8>) -> DMatrix<i16> {
     let (nb_rows, nb_cols) = mat.shape();
     let mut grad_y = DMatrix::zeros(nb_rows, nb_cols);
     if nb_rows >= 2 && nb_cols >= 2 {
-        let inner_grad_y = mat.slice((2, 1), (nb_rows - 2, nb_cols - 2)).zip_map(
-            &mat.slice((0, 1), (nb_rows - 2, nb_cols - 2)),
-            |y_2, y_0| y_2 as i16 - y_0 as i16,
-        );
-        for c in 0..(nb_cols - 2) {
-            for r in 0..(nb_rows - 2) {
-                grad_y[(r + 1, c + 1)] = inner_grad_y[(r, c)];
+        for c in 1..(nb_cols - 1) {
+            for r in 1..(nb_rows - 1) {
+                grad_y[(r, c)] = mat[(r + 1, c)] as i16 - mat[(r - 1, c)] as i16;
             }
         }
     }
