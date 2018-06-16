b3b910ebfa247af88b7362493e54b4d940f6551d
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-06-16 18:02:04 +0800

    Try different gradient implementations

--- examples/gradient.rs
+++ examples/gradient.rs
@@ -4,6 +4,7 @@ extern crate nalgebra as na;
 use image::GrayImage;
 use na::DMatrix;
 use std::path::Path;
+use std::time::{Duration, Instant};
 
 // #[allow(dead_code)]
 fn main() {
@@ -15,6 +16,12 @@ fn main() {
     let img_matrix = matrix_from_image(img);
 
     // Compute discrete spacial gradient.
+    let t_0 = Instant::now();
+    for _ in 0..1000 {
+        let (g_x, g_y) = gradient_2(&img_matrix);
+    }
+    let dt = Instant::now().duration_since(t_0);
+    println!("{:?}", dt);
     let (g_x, g_y) = gradient_2(&img_matrix);
     let g_norm_sqr = g_x.zip_map(&g_y, |gx, gy| {
         (gx as i32 * gx as i32 + gy as i32 * gy as i32) as u16
@@ -36,7 +43,7 @@ fn main() {
 }
 
 fn gradient_2(mat: &DMatrix<u8>) -> (DMatrix<i16>, DMatrix<i16>) {
-    (gradient_x(mat), gradient_y(mat))
+    (gradient_x_inner_from_fn(mat), gradient_y_inner_from_fn(mat))
 }
 
 fn gradient_x(mat: &DMatrix<u8>) -> DMatrix<i16> {
@@ -52,6 +59,23 @@ fn gradient_x(mat: &DMatrix<u8>) -> DMatrix<i16> {
     grad_x
 }
 
+fn gradient_x_inner_from_fn(mat: &DMatrix<u8>) -> DMatrix<i16> {
+    let (nb_rows, nb_cols) = mat.shape();
+    DMatrix::from_fn(nb_rows - 2, nb_cols - 2, |r, c| {
+        mat[(r + 1, c + 2)] as i16 - mat[(r + 1, c)] as i16
+    })
+}
+
+fn gradient_x_inner_slice(mat: &DMatrix<u8>) -> DMatrix<i16> {
+    let (nb_rows, nb_cols) = mat.shape();
+    assert!(nb_rows >= 2);
+    assert!(nb_cols >= 2);
+    mat.slice((1, 2), (nb_rows - 2, nb_cols - 2)).zip_map(
+        &mat.slice((1, 0), (nb_rows - 2, nb_cols - 2)),
+        |x_2, x_0| x_2 as i16 - x_0 as i16,
+    )
+}
+
 fn gradient_y(mat: &DMatrix<u8>) -> DMatrix<i16> {
     let (nb_rows, nb_cols) = mat.shape();
     let mut grad_y = DMatrix::zeros(nb_rows, nb_cols);
@@ -63,6 +87,23 @@ fn gradient_y(mat: &DMatrix<u8>) -> DMatrix<i16> {
         }
     }
     grad_y
+}
+
+fn gradient_y_inner_from_fn(mat: &DMatrix<u8>) -> DMatrix<i16> {
+    let (nb_rows, nb_cols) = mat.shape();
+    DMatrix::from_fn(nb_rows - 2, nb_cols - 2, |r, c| {
+        mat[(r + 2, c + 1)] as i16 - mat[(r, c + 1)] as i16
+    })
+}
+
+fn gradient_y_inner_slice(mat: &DMatrix<u8>) -> DMatrix<i16> {
+    let (nb_rows, nb_cols) = mat.shape();
+    assert!(nb_rows >= 2);
+    assert!(nb_cols >= 2);
+    mat.slice((2, 1), (nb_rows - 2, nb_cols - 2)).zip_map(
+        &mat.slice((0, 1), (nb_rows - 2, nb_cols - 2)),
+        |y_2, y_0| y_2 as i16 - y_0 as i16,
+    )
 }
 
 fn image_from_matrix(mat: &DMatrix<u8>) -> GrayImage {
