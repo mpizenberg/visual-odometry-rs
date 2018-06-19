56485d2c4bc626a9d407e0ed901fe559cad98ab7
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-06-19 17:33:08 +0800

    Try to extract 16 PNG read into module

---
+++ examples/helper/mod.rs
@@ -0,0 +1,28 @@
+extern crate byteorder;
+extern crate nalgebra as na;
+extern crate png;
+
+use byteorder::{BigEndian, ReadBytesExt};
+use png::HasParameters;
+use std::fs::File;
+use std::io::Cursor;
+
+pub fn read_png_16bits(file_path: &str) -> Result<(usize, usize, Vec<u16>), png::DecodingError> {
+    // Load 16 bits PNG depth image.
+    let img_file = File::open(file_path)?;
+    let mut decoder = png::Decoder::new(img_file);
+    // Use the IDENTITY transformation because by default
+    // it will use STRIP_16 which only keep 8 bits.
+    decoder.set(png::Transformations::IDENTITY);
+    let (info, mut reader) = decoder.read_info()?;
+    let mut buffer = vec![0; info.buffer_size()];
+    reader.next_frame(&mut buffer)?;
+
+    // Transform buffer into 16 bits slice.
+    let mut buffer_u16 = vec![0; (info.width * info.height) as usize];
+    let mut buffer_cursor = Cursor::new(buffer);
+    buffer_cursor.read_u16_into::<BigEndian>(&mut buffer_u16)?;
+
+    // Return u16 buffer.
+    Ok((info.width, info.height, buffer_u16))
+}
--- examples/png_16bits.rs
+++ examples/png_16bits.rs
@@ -1,44 +1,44 @@
-extern crate byteorder;
+// extern crate byteorder;
 extern crate nalgebra as na;
-extern crate png;
+// extern crate png;
 
-use byteorder::{BigEndian, ReadBytesExt};
+mod helper;
+
+// use byteorder::{BigEndian, ReadBytesExt};
 use na::DMatrix;
-use png::HasParameters;
-use std::fs::File;
-use std::io::Cursor;
+// use png::HasParameters;
+// use std::fs::File;
+// use std::io::Cursor;
 
 // #[allow(dead_code)]
 fn main() {
-    // Load 16 bits PNG depth image.
-    let img_file = File::open("icl-depth/0.png").unwrap();
-    let mut decoder = png::Decoder::new(img_file);
-    // Use the IDENTITY transformation because by default
-    // it will use STRIP_16 which only keep 8 bits.
-    decoder.set(png::Transformations::IDENTITY);
-    let (info, mut reader) = decoder.read_info().unwrap();
-    let mut buffer = vec![0; info.buffer_size()];
-    reader.next_frame(&mut buffer).unwrap();
+    // // Load 16 bits PNG depth image.
+    // let img_file = File::open("icl-depth/0.png").unwrap();
+    // let mut decoder = png::Decoder::new(img_file);
+    // // Use the IDENTITY transformation because by default
+    // // it will use STRIP_16 which only keep 8 bits.
+    // decoder.set(png::Transformations::IDENTITY);
+    // let (info, mut reader) = decoder.read_info().unwrap();
+    // let mut buffer = vec![0; info.buffer_size()];
+    // reader.next_frame(&mut buffer).unwrap();
+    //
+    // // Display image metadata.
+    // println!("info: {:?}", info.width);
+    // println!("height: {:?}", info.height);
+    // println!("bit depth: {:?}", info.bit_depth);
+    // println!("buffer size: {:?}", info.buffer_size());
+    //
+    // // Transform buffer into 16 bits slice.
+    // let mut buffer_u16 = vec![0; (info.width * info.height) as usize];
+    // let mut buffer_cursor = Cursor::new(buffer);
+    // buffer_cursor
+    //     .read_u16_into::<BigEndian>(&mut buffer_u16)
+    //     .unwrap();
 
-    // Display image metadata.
-    println!("info: {:?}", info.width);
-    println!("height: {:?}", info.height);
-    println!("bit depth: {:?}", info.bit_depth);
-    println!("buffer size: {:?}", info.buffer_size());
-
-    // Transform buffer into 16 bits slice.
-    let mut buffer_u16 = vec![0; (info.width * info.height) as usize];
-    let mut buffer_cursor = Cursor::new(buffer);
-    buffer_cursor
-        .read_u16_into::<BigEndian>(&mut buffer_u16)
-        .unwrap();
-
+    // Read 16 bits PNG image.
+    let (width, height, buffer_u16) = helper::read_png_16bits("icl-depth/0.png").unwrap();
     // Transform it into a matrix.
-    let img_mat: DMatrix<u16> = DMatrix::from_row_slice(
-        info.height as usize,
-        info.width as usize,
-        buffer_u16.as_slice(),
-    );
+    let img_mat: DMatrix<u16> = DMatrix::from_row_slice(height, width, buffer_u16.as_slice());
     let img_slice = img_mat.slice((0, 0), (3, 3));
     println!("Top left: {}", img_slice);
 }
