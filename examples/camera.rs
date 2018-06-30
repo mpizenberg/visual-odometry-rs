e82e64fa5dfe65dc284f01092e306fe80525110f
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-06-30 13:19:59 +0800

    Clean camera example (but not working)

--- examples/camera.rs
+++ examples/camera.rs
@@ -1,65 +1,45 @@
 extern crate computer_vision_rs as cv;
 extern crate nalgebra;
 
-// use cv::camera::{Extrinsics, Intrinsics, Pinhole, Quaternions};
-use nalgebra::{Matrix4, Projective3, Quaternion, RowVector4, UnitQuaternion};
+use cv::camera::{Camera, Extrinsics, Intrinsics};
+use nalgebra::{Point2, Quaternion, Translation3, UnitQuaternion};
 
 fn main() {
-    // let intrinsics = Intrinsics::Pinhole(Pinhole {
-    //     principal_point: (319.5, 239.5),
-    //     focal_length: 1.0,
-    //     scaling: (481.20, 480.00),
-    //     skew: 0.0,
-    // });
-    let proj_0 = Projective3::from_matrix_unchecked(Matrix4::from_rows(&[
-        RowVector4::new(-0.999762, 0.0, -0.021799, 1.370500),
-        RowVector4::new(0.0, 1.0, 0.0, 1.517390),
-        RowVector4::new(0.021799, 0.0, -0.999762, 1.449630),
-        RowVector4::new(0.0, 0.0, 0.0, 1.0),
-    ]));
-    let proj_1 = Projective3::from_matrix_unchecked(Matrix4::from_rows(&[
-        RowVector4::new(-0.999738, -0.000418, -0.022848, 1.370020),
-        RowVector4::new(-0.000464, 0.999998, 0.002027, 1.526344),
-        RowVector4::new(0.022848, 0.002037, -0.999737, 1.448990),
-        RowVector4::new(0.0, 0.0, 0.0, 1.0),
-    ]));
-    let proj_50 = Projective3::from_matrix_unchecked(Matrix4::from_rows(&[
-        RowVector4::new(-0.980396, 0.089427, -0.175573, 1.244141),
-        RowVector4::new(0.087771, 0.995992, 0.017191, 1.526088),
-        RowVector4::new(0.176407, 0.001444, -0.984316, 1.457656),
-        RowVector4::new(0.0, 0.0, 0.0, 1.0),
-    ]));
-    let proj_600 = Projective3::from_matrix_unchecked(Matrix4::from_rows(&[
-        RowVector4::new(-0.733957, 0.247028, -0.632679, 1.043566),
-        RowVector4::new(0.311943, 0.950058, 0.009070, 1.085034),
-        RowVector4::new(0.603322, -0.190703, -0.774360, 0.687636),
-        RowVector4::new(0.0, 0.0, 0.0, 1.0),
-    ]));
-    let proj_01 = proj_1 * proj_0.inverse();
-    let proj_050 = proj_50 * proj_0.inverse();
-    let proj_0600 = proj_600 * proj_0.inverse();
-    // println!("Projection 0 to 1: {}", proj_01.unwrap());
-    // println!("Projection 0 to 50: {}", proj_050.unwrap());
-    println!("Projection 0 to 600: {}", proj_0600.unwrap());
-    let orientation_0 = UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0));
-    let orientation_1 = UnitQuaternion::from_quaternion(Quaternion::new(
-        0.999999,
-        -0.00101358,
-        0.00052453,
-        -0.000231475,
-    ));
-    let orientation_50 = UnitQuaternion::from_quaternion(Quaternion::new(
-        0.995981,
-        -0.00516676,
-        0.0775787,
-        0.0444655,
-    ));
-    let orientation_600 =
+    let intrinsics = Intrinsics {
+        principal_point: (319.5, 239.5),
+        focal_length: 1.0,
+        scaling: (481.20, 480.00),
+        skew: 0.0,
+    };
+
+    // Camera at frame 0.
+    let translation_1 = Translation3::new(0.0, 0.0, -2.25);
+    let rotation_1 = UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0));
+    let extrinsics_1 = Extrinsics::new(translation_1, rotation_1);
+    let camera_1 = Camera::new(intrinsics.clone(), extrinsics_1);
+
+    // Camera at frame 600.
+    let translation_600 = Translation3::new(0.310245, -0.43235, -1.48106);
+    let rotation_600 =
         UnitQuaternion::from_quaternion(Quaternion::new(0.933403, 0.0471923, 0.322162, 0.150811));
-    let rotation_01 = orientation_0.rotation_to(&orientation_1);
-    let rotation_050 = orientation_0.rotation_to(&orientation_50);
-    let rotation_0600 = orientation_0.rotation_to(&orientation_600);
-    // println!("Rotation 0 to 1: {}", rotation_01.to_rotation_matrix());
-    // println!("Rotation 0 to 50: {}", rotation_050.to_rotation_matrix());
-    println!("Rotation 0 to 600: {}", rotation_0600.to_rotation_matrix());
+    let extrinsics_600 = Extrinsics::new(translation_600, rotation_600);
+    let camera_600 = Camera::new(intrinsics.clone(), extrinsics_600);
+
+    // Bottom left frame corner in frame 0.
+    let bl_pos_1 = Point2::new(386.0, 278.0);
+    let bl_depth_1 = 1682.0 / 5000.0;
+
+    // Bottom left frame corner in frame 600.
+    let bl_pos_600 = Point2::new(14.0, 102.0);
+    let bl_depth_600 = 1067.0 / 5000.0;
+
+    // Project bottom left corner from frame 0 to frame 600.
+    println!(
+        "Back Projected 1: {}",
+        camera_1.back_project(bl_pos_1, bl_depth_1)
+    );
+    println!(
+        "Back Projected 600: {}",
+        camera_600.back_project(bl_pos_600, bl_depth_600)
+    );
 }
--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,29 +1,97 @@
+use nalgebra::{Affine2, Matrix3, Point2, Point3, Translation3, UnitQuaternion, Vector3};
+
 type Float = f32;
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub enum Intrinsics {
-    Pinhole(Pinhole),
-    PinholeWithDistortion(Pinhole, RadialTangential),
-    FishEye(FishEye),
-    Fov(Fov),
+#[derive(PartialEq, Debug, Clone)]
+pub struct Camera {
+    pub intrinsics: Intrinsics,
+    pub extrinsics: Extrinsics,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct Pinhole {
+impl Camera {
+    pub fn new(intrinsics: Intrinsics, extrinsics: Extrinsics) -> Camera {
+        Camera {
+            intrinsics,
+            extrinsics,
+        }
+    }
+
+    pub fn project(&self, point: Point3<Float>) -> Vector3<Float> {
+        self.intrinsics.project(self.extrinsics.project(point))
+    }
+
+    pub fn back_project(&self, point: Point2<Float>, depth: Float) -> Point3<Float> {
+        self.extrinsics
+            .back_project(self.intrinsics.back_project(point, depth))
+    }
+}
+
+// EXTRINSICS ##############################################
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Extrinsics {
+    pub translation: Translation3<Float>,
+    pub rotation: UnitQuaternion<Float>,
+}
+
+impl Extrinsics {
+    pub fn new(translation: Translation3<Float>, rotation: UnitQuaternion<Float>) -> Extrinsics {
+        Extrinsics {
+            translation,
+            rotation,
+        }
+    }
+
+    pub fn project(&self, point: Point3<Float>) -> Point3<Float> {
+        self.translation * (self.rotation * point)
+    }
+
+    pub fn back_project(&self, point: Point3<Float>) -> Point3<Float> {
+        self.rotation.inverse() * (self.translation.inverse() * point)
+    }
+}
+
+// INTRINSICS ##############################################
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Intrinsics {
     pub principal_point: (Float, Float),
     pub focal_length: Float,
     pub scaling: (Float, Float),
     pub skew: Float,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct RadialTangential {
-    pub radial_coeffs: (Float, Float, Float),
-    pub tangential_coeffs: (Float, Float),
+impl Intrinsics {
+    pub fn matrix(&self) -> Affine2<Float> {
+        Affine2::from_matrix_unchecked(Matrix3::new(
+            self.focal_length * self.scaling.0,
+            self.skew,
+            self.principal_point.0,
+            0.0,
+            self.focal_length * self.scaling.1,
+            self.principal_point.1,
+            0.0,
+            0.0,
+            1.0,
+        ))
+    }
+
+    pub fn project(&self, point: Point3<Float>) -> Vector3<Float> {
+        Vector3::new(
+            self.focal_length * self.scaling.0 * point[0] + self.skew * point[1]
+                + self.principal_point.0 * point[2],
+            self.focal_length * self.scaling.1 * point[1] + self.principal_point.1 * point[2],
+            point[2],
+        )
+    }
+
+    pub fn back_project(&self, point: Point2<Float>, depth: Float) -> Point3<Float> {
+        let z = depth;
+        let y = (point[1] - self.principal_point.1) * z / (self.focal_length * self.scaling.1);
+        let x = ((point[0] - self.principal_point.0) * z - self.skew * y)
+            / (self.focal_length * self.scaling.0);
+        Point3::new(x, y, z)
+    }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct FishEye {}
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-pub struct Fov {}
+// FUNCTIONS ###############################################
