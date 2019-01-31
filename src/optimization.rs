586d0784980363731b1ddf7c2975b0a6937eabdf
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-01-31 15:13:48 +0100

    Clean optimization module

--- src/optimization.rs
+++ src/optimization.rs
@@ -2,49 +2,51 @@ use std::f32;
 
 pub type Float = f32;
 
-// An interative optimizer where each part of the algorithm is personalizable.
-pub fn iterative<
-    Params,       // Parameters of optimizer, such as \lambda of Levenberg Marquardt
-    Observations, // The observations, i.e. the data
-    Model,        // Parametrization of the modelization such as a struct
-    Residuals,    // The differences between the projection of the model and the observed data
-    Jacobian,     // Jacobian of the residuals, typically an NxM matrix (N data, M variables)
-    Gradient,     // Typically the matrix: Jacobian^T * Residuals
-    EvalFn,       // Function evaluating the jacobian and residuals
-    StepFn,       // Function computing the iteration step
-    CriterionFn,  // Stopping criterion
->(
-    eval: EvalFn,
-    _step: StepFn,
-    _stop_criterion: CriterionFn,
-    observations: &Observations,
-    initial_model: Model,
-    initial_params: Params,
-) -> (Model, usize)
-where
-    EvalFn: Fn(&Observations, &Model) -> (Jacobian, Gradient, Residuals, Float),
-    StepFn: Fn(&Jacobian, &Residuals, &Model, &Params) -> (Model, Params),
-    CriterionFn: Fn(
-        usize,
-        (Params, Float, Gradient, Model),
-        (Params, Float, Gradient, Model),
-    ) -> ((Params, Float, Gradient, Model), Continue),
-{
-    // Evaluate the model for the first time.
-    let (_jacobian, gradient, _residuals, energy) = eval(observations, &initial_model);
-    let _state = (initial_params, energy, gradient, initial_model);
-    let mut nb_iter = 0;
-
-    loop {
-        nb_iter = nb_iter + 1;
-        // let (model, params) = step(&jacobian, &residuals, &state.3, &state.0);
-        // let (jacobian, gradient, residuals, energy) = eval(observations, &model);
-        // let step_state = (params, energy, gradient, model);
-        // let (kept_state, continuation) = stop_criterion(nb_iter, state, step_state);
-    }
-}
-
 pub enum Continue {
     Stop,
     Forward,
 }
+
+pub struct State<Params, Float, Model, Residuals, Jacobian, Gradient> {
+    params: Params,
+    energy: Float,
+    model: Model,
+    residuals: Residuals,
+    jacobian: Jacobian,
+    gradient: Gradient,
+}
+
+pub trait QuasiNewtonOptimizer<Observations, P, F, M, R, J, G> {
+    fn eval(observations: &Observations, model: M) -> State<P, F, M, R, J, G>;
+    fn step(state: &State<P, F, M, R, J, G>) -> M;
+    fn stop_criterion(
+        nb_iter: usize,
+        old_state: State<P, F, M, R, J, G>,
+        new_state: State<P, F, M, R, J, G>,
+    ) -> (State<P, F, M, R, J, G>, Continue);
+
+    fn iterative(
+        observations: &Observations,
+        mut state: State<P, F, M, R, J, G>,
+    ) -> (State<P, F, M, R, J, G>, usize) {
+        let mut nb_iter = 0;
+        loop {
+            nb_iter = nb_iter + 1;
+            let new_model = Self::step(&state);
+            let new_state = Self::eval(observations, new_model);
+            let (kept_state, continuation) = Self::stop_criterion(nb_iter, state, new_state);
+            state = kept_state;
+            if let Continue::Stop = continuation {
+                return (state, nb_iter);
+            }
+        }
+    }
+}
+
+// pub struct GaussNewtonOptimizer;
+//
+// impl QuasiNewtonOptimizer for GaussNewtonOptimizer {
+//     eval
+// }
+//
+// GaussNewtonOptimizer::iterative(state)
