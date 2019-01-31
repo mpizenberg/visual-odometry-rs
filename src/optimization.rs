79193e5b1e0ed8c68451d3472d713ad911a9bcca
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-01-31 12:05:46 +0100

    Rewrite (not finished) attempt

--- src/optimization.rs
+++ src/optimization.rs
@@ -2,61 +2,49 @@ use std::f32;
 
 pub type Float = f32;
 
-pub fn iterative<Observation, Model, Derivatives, Residual, EvalFn, StepFn, CriterionFn>(
+// An interative optimizer where each part of the algorithm is personalizable.
+pub fn iterative<
+    Params,       // Parameters of optimizer, such as \lambda of Levenberg Marquardt
+    Observations, // The observations, i.e. the data
+    Model,        // Parametrization of the modelization such as a struct
+    Residuals,    // The differences between the projection of the model and the observed data
+    Jacobian,     // Jacobian of the residuals, typically an NxM matrix (N data, M variables)
+    Gradient,     // Typically the matrix: Jacobian^T * Residuals
+    EvalFn,       // Function evaluating the jacobian and residuals
+    StepFn,       // Function computing the iteration step
+    CriterionFn,  // Stopping criterion
+>(
     eval: EvalFn,
     step: StepFn,
     stop_criterion: CriterionFn,
-    observation: &Observation,
+    observations: &Observations,
     initial_model: Model,
+    initial_params: Params,
 ) -> (Model, usize)
 where
-    EvalFn: Fn(&Observation, &Model) -> (Derivatives, Residual),
-    StepFn: Fn(&Derivatives, &Residual, &Model) -> Model,
-    CriterionFn: Fn(usize, Float, &Residual) -> (Float, Continue),
+    EvalFn: Fn(&Observations, &Model) -> (Jacobian, Gradient, Residuals, Float),
+    StepFn: Fn(&Jacobian, &Residuals, &Model, &Params) -> (Model, Params),
+    CriterionFn: Fn(
+        usize,
+        (Params, Float, Gradient, Model),
+        (Params, Float, Gradient, Model),
+    ) -> ((Params, Float, Gradient, Model), Continue),
 {
-    // Manual first iteration enable avoiding to have Clone for model.
-    // Otherwise, the compiler doesn't know if previous_model has
-    // been initialized in the backward branch.
-    let mut energy = f32::INFINITY;
-    let (mut derivatives, mut residual) = eval(observation, &initial_model);
-    match stop_criterion(0, energy, &residual) {
-        (new_energy, Continue::Forward) => {
-            energy = new_energy;
-        }
-        _ => return (initial_model, 0),
-    }
-    let mut nb_iter = 1;
-    let mut model = step(&derivatives, &residual, &initial_model);
-    let mut previous_model = initial_model;
-    let (new_derivatives, new_residual) = eval(observation, &model);
-    derivatives = new_derivatives;
-    residual = new_residual;
+    // Evaluate the model for the first time.
+    let (jacobian, gradient, residuals, energy) = eval(observations, &initial_model);
+    let mut state = (initial_params, energy, gradient, initial_model);
+    let mut nb_iter = 0;
 
-    // After first iteration, loop until stop criterion.
     loop {
-        let (new_energy, continuation) = stop_criterion(nb_iter, energy, &residual);
-        match continuation {
-            Continue::Stop => break,
-            Continue::Backward => {
-                model = previous_model;
-                break;
-            }
-            Continue::Forward => {
-                nb_iter = nb_iter + 1;
-                energy = new_energy;
-                previous_model = model;
-                model = step(&derivatives, &residual, &previous_model);
-                let (new_derivatives, new_residual) = eval(observation, &model);
-                derivatives = new_derivatives;
-                residual = new_residual;
-            }
-        }
+        nb_iter = nb_iter + 1;
+        let (model, params) = step(&jacobian, &residuals, &state.3, &state.0);
+        let (jacobian, gradient, residuals, energy) = eval(observations, &model);
+        let step_state = (params, energy, gradient, model);
+        let (kept_state, continuation) = stop_criterion(nb_iter, state, step_state);
     }
-    (model, nb_iter)
 }
 
 pub enum Continue {
     Stop,
     Forward,
-    Backward,
 }
