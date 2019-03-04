883b355bad843721a28f449f8621b4984d41de23
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-03-04 19:31:36 +0100

    Doc core/track/lm_optimizer.rs

--- src/core/track/lm_optimizer.rs
+++ src/core/track/lm_optimizer.rs
@@ -1,3 +1,6 @@
+//! Levenberg-Marquardt implementation of the Optimizer trait
+//! for the inverse compositional tracking algorithm.
+
 use nalgebra::{DMatrix, UnitQuaternion};
 use std::f32;
 
@@ -6,26 +9,54 @@ use crate::math::optimizer::{self as optim, Optimizer, State};
 use crate::math::se3;
 use crate::misc::type_aliases::{Float, Iso3, Mat6, Point2, Vec6};
 
+/// Empty struct, implementor of `Optimizer`.
 pub struct LMOptimizer;
+
+/// State of the Levenberg-Marquardt optimizer.
 pub struct LMState {
+    /// Levenberg-Marquardt hessian diagonal coefficient.
     pub lm_coef: Float,
+    /// Data used during the optimizer iterations.
     pub data: LMData,
 }
+
+/// Data needed during the optimizer iterations.
 pub struct LMData {
+    /// The hessian matrix of the system.
     pub hessian: Mat6,
+    /// The gradient of the system.
     pub gradient: Vec6,
+    /// Energy associated with the current model.
     pub energy: Float,
+    /// Estimated pose at the current state of iterations.
     pub model: Iso3,
 }
+
+/// Either a fully constructed `LMData`
+/// or an error containing the energy of the new iteration.
+///
+/// The error is returned when the new computed energy
+/// is higher than the previous iteration energy.
 pub type LMPartialState = Result<LMData, Float>;
+
+/// (inside_indices, residuals, energy).
 pub type PreEval = (Vec<usize>, Vec<Float>, Float);
+
+/// Precomputed data available for the optimizer iterations:
 pub struct Obs<'a> {
+    /// Intrinsic parameters of the camera.
     pub intrinsics: &'a Intrinsics,
+    /// Reference ("keyframe") image.
     pub template: &'a DMatrix<u8>,
+    /// Current image to track.
     pub image: &'a DMatrix<u8>,
+    /// Coordinates of the points used for the tracking.
     pub coordinates: &'a Vec<(usize, usize)>,
+    /// Inverse depth of the points used for the tracking.
     pub _z_candidates: &'a Vec<Float>,
+    /// Jacobians precomputed for the points used for the tracking.
     pub jacobians: &'a Vec<Vec6>,
+    /// Hessian matrices precomputed for the points used for the tracking.
     pub hessians: &'a Vec<Mat6>,
 }
 
@@ -38,11 +69,15 @@ impl State<Iso3, Float> for LMState {
     }
 }
 
+/// impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float> for LMOptimizer.
 impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float> for LMOptimizer {
+    /// Initial energy is f32::INFINITY.
     fn initial_energy() -> Float {
         f32::INFINITY
     }
 
+    /// Compute the step using Levenberg-Marquardt.
+    /// May return an error at the Cholesky decomposition of the hessian.
     fn compute_step(state: &LMState) -> Option<Vec6> {
         let mut hessian = state.data.hessian.clone();
         hessian.m11 = (1.0 + state.lm_coef) * hessian.m11;
@@ -54,6 +89,7 @@ impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float>
         hessian.cholesky().map(|ch| ch.solve(&state.data.gradient))
     }
 
+    /// Apply the step of an inverse compositional approach to compute the next pose estimation.
     fn apply_step(delta: Vec6, model: &Iso3) -> Iso3 {
         let delta_warp = se3::exp(delta);
         let mut not_normalized = model * delta_warp.inverse();
@@ -61,6 +97,7 @@ impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float>
         not_normalized
     }
 
+    /// Compute residuals and energy of the new model.
     fn pre_eval(obs: &Obs, model: &Iso3) -> PreEval {
         let mut inside_indices = Vec::new();
         let mut residuals = Vec::new();
@@ -82,6 +119,7 @@ impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float>
         (inside_indices, residuals, energy)
     }
 
+    /// Evaluate the new hessian and gradient if the energy is lower than previously.
     fn eval(obs: &Obs, energy: Float, pre_eval: PreEval, model: Iso3) -> LMPartialState {
         let (inside_indices, residuals, new_energy) = pre_eval;
         if new_energy > energy {
@@ -105,6 +143,11 @@ impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float>
         }
     }
 
+    /// Stop after too many iterations,
+    /// or if the energy variation is too low.
+    ///
+    /// Also updates the Levenberg-Marquardt coefficient
+    /// depending on if the energy increased or decreased.
     fn stop_criterion(
         nb_iter: usize,
         s0: LMState,
@@ -153,12 +196,14 @@ impl<'a> Optimizer<Obs<'a>, LMState, Vec6, Iso3, PreEval, LMPartialState, Float>
 
 // Helper ######################################################################
 
+/// First order Taylor approximation for unit quaternion re-normalization.
 fn re_normalize(uq: UnitQuaternion<Float>) -> UnitQuaternion<Float> {
     let q = uq.into_inner();
     let sq_norm = q.norm_squared();
     UnitQuaternion::new_unchecked(0.5 * (3.0 - sq_norm) * q)
 }
 
+/// Warp a point from an image to another by a given rigid body motion.
 fn warp(model: &Iso3, x: Float, y: Float, _z: Float, intrinsics: &Intrinsics) -> (Float, Float) {
     let x1 = intrinsics.back_project(Point2::new(x, y), 1.0 / _z);
     let x2 = model * x1;
@@ -166,6 +211,8 @@ fn warp(model: &Iso3, x: Float, y: Float, _z: Float, intrinsics: &Intrinsics) ->
     (uvz2.x / uvz2.z, uvz2.y / uvz2.z)
 }
 
+/// Simple linear interpolation of a pixel with floating point coordinates.
+/// Return `None` if the point is outside of the image boundaries.
 fn interpolate(x: Float, y: Float, image: &DMatrix<u8>) -> Option<Float> {
     let (height, width) = image.shape();
     let u = x.floor();
--- src/core/track/mod.rs
+++ src/core/track/mod.rs
@@ -1,2 +1,4 @@
+//! Useful types and functions for tracking a camera.
+
 pub mod inverse_compositional;
 pub mod lm_optimizer;
