b48ec5f461167aa25d5272c1d55891fea97c1f11
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-03-22 08:33:43 +0100

    Add license header to all source code

--- src/core/track/inverse_compositional.rs
+++ src/core/track/inverse_compositional.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! Types and functions to implement an inverse compositional tracking algorithm.
 //!
 //! Implementation of "Lucas-kanade 20 years on: A unifying framework"
--- src/math/optimizer.rs
+++ src/math/optimizer.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! Guiding traits to implement iterative optimization algorithms.
 
 /// Enum used to indicate if iterations should continue or stop.
